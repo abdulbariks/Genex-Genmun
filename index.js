ba0cf537212f768ad054c163a27499806803bc67
Abdul Barik <abdulbarik1997m@gmail.com>
2025-03-04 03:00:16 +0600

    genex genmun design

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("logout-btn").addEventListener("click", function () {
+  window.location.href = "http://172.16.124.11/GenMon/index.php";
+});
+
+document.getElementById("session-clear").addEventListener("click", function () {
+  window.location.href = "http://172.16.124.11/GenMon/index.php";
+});
